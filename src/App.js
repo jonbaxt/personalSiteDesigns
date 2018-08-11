ab102239b82417164555835a40fbe6563e75ee5c
jonbaxt <jonbaxt@gmail.com>
2018-08-11 02:44:57 -0600

    Hid NavBar while Developing more

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+// import { NavHashLink as NavLink } from 'react-router-hash-link';
 import fontawesome from '@fortawesome/fontawesome';
 import brands from '@fortawesome/fontawesome-free-brands';
 import solid from '@fortawesome/fontawesome-free-solid';
 import regular from '@fortawesome/fontawesome-free-regular';
 
-import NavBar from './Components/NavBar/NavBar';
+// import NavBar from './Components/NavBar/NavBar';
 import TopSection from './Components/TopSection/TopSection';
 import AboutSection from './Components/AboutSection/AboutSection';
 import SkillsSection from './Components/Skills/SkillsSection';
@@ -18,11 +19,17 @@ class App extends Component {
     fontawesome.library.add(brands, solid, regular)
     return (
       <div className="App">
-        <NavBar /> 
+        
+        {/* <NavBar />  */}
+        
+        {/* <NavLink to='/#TopSection' ></NavLink> */}
         <TopSection />
         <AboutSection />       
+        
         <SkillsSection /> 
+        
         <Portfolio />
+        
         <Contact />
       </div>
     );
--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+// import { HashLink as Link } from 'react-router-hash-link';
 import './NavBar.css';
 
 export default function NavBar() {
     return (
         <div id='navMain'>
             <div id='navInner'>
+            {/* <Link to='/#TopSection' > */}
             <h3 className='cases'>Home</h3>
+            {/* </Link> */}
             <h3 className='cases'>About</h3>
             <h3 className='cases'>Skills</h3>
             <h3 className='cases'>Portfolio</h3>
