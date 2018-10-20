6a74c082b4bd30f1b87e81108206499b6b62dce1
jonbaxt <jonbaxt@gmail.com>
2018-10-19 22:54:24 -0600

    Disabled most of site to focus on reworking everything, CSS, layout

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ import "animate.css/animate.min.css";
 
 // import NavBar from './Components/NavBar/NavBar';
 import TopSection from './Components/TopSection/TopSection';
-import AboutSection from './Components/AboutSection/AboutSection';
-import SkillsSection from './Components/Skills/SkillsSection';
-import Portfolio from './Components/Portfolio/Portfolio';
-import Contact from './Components/Contact/Contact';
+// import AboutSection from './Components/AboutSection/AboutSection';
+// import SkillsSection from './Components/Skills/SkillsSection';
+// import Portfolio from './Components/Portfolio/Portfolio';
+// import Contact from './Components/Contact/Contact';
 
 import './App.css';
 
@@ -27,13 +27,13 @@ class App extends Component {
         
         {/* <NavLink to='/#TopSection' ></NavLink> */}
         <TopSection />
-        <AboutSection />       
+        {/* <AboutSection />        */}
         
-        <SkillsSection /> 
+        {/* <SkillsSection />  */}
         
-        <Portfolio />
+        {/* <Portfolio /> */}
         
-        <Contact />
+        {/* <Contact /> */}
       </div>
     );
   }
